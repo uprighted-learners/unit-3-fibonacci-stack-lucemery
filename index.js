1ce31fa4f17741aa2f60d93dd8bcb5c802238b16
lucemery <lucemery316@gmail.com>
2024-05-10 17:35:39 -0500

    finished lab w./ help from Max

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // Create the `series` variable
 var series = [0, 1];
 
-while (/* your check here */) {
+while (series.length < 10) {
   // your code here
+  let x = series.pop();
+  let y = series.pop();
+
+  let sum = x + y;
+// need to not push first variable first
+  series.push(y, x, sum);
 }
+
+// console.log(x)
+
 
 // Print the Fibonacci sequence to the console
 console.log(series);
